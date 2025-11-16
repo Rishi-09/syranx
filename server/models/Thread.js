c007a48ee3ad4d5e523c0c277867843761aaa6ef
Rishi <rishikm215@gmail.com>
2025-11-16 22:09:23 +0530

    added message and thread shcema

---
+++ server/models/Thread.js
@@ -0,0 +1,44 @@
+import mongoose, { mongo } from "mongoose";
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  role: {
+    type: String,
+    enum: ["user", "assistant"],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ThreadSchema = new Schema({
+  threadId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    default: "New Chat",
+  },
+  message: [MessageSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+
+
+export default mongoose.model("Thread",ThreadSchema);
