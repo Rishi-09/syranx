cce2feb32fac4b3b532116f29af926934fbd6f77
Rishi <rishikm215@gmail.com>
2025-11-20 20:00:23 +0530

    customised sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,38 +5,34 @@ const Sidebar = () => {
     <>
       <section className="h-screen w-56 bg-neutral-800  ">
         {/*new chat section */}
-        <div className="m-4">
-          <i className="fa-solid fa-chess-queen "></i>
-          <p className="inline p-4">Syranx</p>
+        <div className=" flex border border-zinc-400/30 mt-2 m-1 rounded-md">
+          <i className="fa-solid fa-chess-queen mt-2.5 ml-4  mb-1  "></i>
+          <p className="inline  mt-1 ml-16  text-xl mb-1 ">Syranx</p>
         </div>
-        <button className="m-4 pt-1.5 pr-3 pl-3 pb-1.5 rounded-2xl  hover:bg-amber-100/10 transition-all duration-700 ">
+        <div className="mt-6 mb-3" >
+          <a className="hover:bg-amber-100/10   pt-2 m-3 pr-10  pl-3 pb-2  rounded-md transition-all duration-200 " href="#" >
           {/* <i class="fa-regular fa-pen-to-square" style="color: #ffffff;"></i> */}
+          <i class="fa-regular fa-pen-to-square mr-5"></i>
           New Chat
-        </button>
+        </a>
+        </div>
         <hr className="opacity-10 " />
-        <ul className="p-4">
-          <li>
-            <button className="hover:bg-amber-100/10 rounded-2xl pr-3 pl-3 pt-1 pb-1 transition-all duration-700">
-              history1
-            </button>
+        <ul className="">
+          <li className="w-full ml-1 mt-3 mb-2 " >
+            <a href="#" className="hover:bg-amber-100/10  w-11/12 pt-2 m-3 pr-22 pl-3 pb-2  rounded-md transition-all duration-200" >threads</a>
+          </li  >
+          <li className="w-full ml-1 mt-2 mb-2"  >
+          <a href="#" className="hover:bg-amber-100/10  w-11/12 pt-2 m-3 pr-22 pl-3 pb-2  rounded-md transition-all duration-200" >threads</a>
           </li>
-          <li>
-            <button className="hover:bg-amber-100/10 rounded-2xl pr-3 pl-3 pt-1 pb-1 transition-all duration-700">
-              history2
-            </button>
+          <li className="w-full ml-1 mt-2 mb-2"  >
+            <a href="#" className="hover:bg-amber-100/10  w-11/12 pt-2  m-3 pr-22 pl-3 pb-2  rounded-md transition-all duration-200" >threads</a>
           </li>
-          <li>
-            <button className="hover:bg-amber-100/10 rounded-2xl pr-3 pl-3 pt-1 pb-1 transition-all duration-700">
-              history3
-            </button>
-          </li>
-          <li>
-            <button className="hover:bg-amber-100/10 rounded-2xl pr-3 pl-3 pt-1 pb-1 transition-all duration-700">
-              history4
-            </button>
+          <li className="w-full ml-1 mt-2 mb-2"  >
+            <a href="#" className="hover:bg-amber-100/10  w-11/12 pt-2 m-3 pr-22 pl-3 pb-2  rounded-md transition-all duration-200" >threads</a>
           </li>
         </ul>
-        <hr className="opacity-10 " />
+        
+        <hr className="opacity-10 mt-5" />
         <div className="p-5 absolute bottom-0 ">By Rishi</div>
       </section>
     </>
