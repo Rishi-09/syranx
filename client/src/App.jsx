c4357326adb8bc9bde409dec2eb9b7476841c8c4
Rishi <rishikm215@gmail.com>
2025-11-22 19:29:20 +0530

    modified App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Home from "./pages/Home";
 import Sidebar from "./components/Sidebar";
 import Chatwindow from "./components/Chatwindow";
 import { Mycontext } from "./components/Mycontext";
+import {v1 as uuid} from 'uuid';
 const App = () => {
-  axios
-    .get("http://localhost:8080/", { withCredentials: true })
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-  const providerValues = {};
+  // axios
+  //   .get("http://localhost:8080/api/thread", { withCredentials: true })
+  //   .then((data) => console.log(data))
+  //   .catch((err) => console.log(err));
+
+  let [prompt,setPrompt] = useState("");
+  let [reply,setReply] = useState(null);
+  let [currThreadId,setcurrThreadId] = useState(uuid);
+  const providerValues = {
+    prompt,setPrompt,
+    reply,setReply,
+    currThreadId,setcurrThreadId
+  };
   return (
     <div className="main flex ">
-      <Mycontext.Provider values={providerValues}>
+      <Mycontext.Provider value={providerValues}>
         <Sidebar />
         <Chatwindow />
       </Mycontext.Provider>
