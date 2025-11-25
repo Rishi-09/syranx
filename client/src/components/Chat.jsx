533fc53c308bca41d59f03878bdb2ad2704584f7
Rishi <rishikm215@gmail.com>
2025-11-25 14:15:08 +0530

    chat component

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,33 +1,39 @@
 import React, { useContext } from "react";
 import { Mycontext } from "./Mycontext";
-
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import 'highlight.js/styles/github-dark.css';
 const Chat = () => {
   let { prevChats, setPrevChats, newChat, setNewChat } = useContext(Mycontext);
-  const alignChat = () => {
-    let userMessage = document.getElementById("userMessage");
-    let aiReply = document.getElementById("aiReply");
-    let i = 0;
-    prevChats.map((chat) => {
-      if (chat[i].role === "user") {
-        userMessage.className = userMessage.className + " absolute right-0 ";
-      } else {
-        aiReply.className = aiReply.className + " absolute left-0 ";
-      }
-    });
-  };
   return (
     <>
       <div className="chat ">
-        <div id="userMessage" className="flex justify-end">
-          <p className="prompt m-4 p-4 bg-amber-50/10 rounded-4xl  ">
-            prompt
-          </p>
-        </div>
-        <div id="aiReply" className="flex justify-start" >
-          <p className="reply m-4 max-w-5/6 p-4 bg-amber-50/10 rounded-4xl " >
-            aiReply Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, error?
-          </p>
-        </div>
+        {prevChats?.map((chat, idx) => (
+          <div
+            className={
+              chat.role === "user" ? "flex justify-end" : "flex justify-start"
+            }
+            key={idx}
+          >
+            {chat.role === "user" ? (
+              <p className="prompt m-4 p-4 bg-amber-50/10 rounded-4xl  ">
+                {chat.content}
+              </p>
+            ) : (
+              <div className="reply m-4 max-w-5/6 p-4 rounded-4xl" >
+                <ReactMarkdown
+                rehypePlugins={[rehypeHighlight]}
+                
+              >
+                {typeof chat.content === "string"
+                  ? chat.content
+                  : String(chat.content)}
+              </ReactMarkdown>
+              </div>
+              
+            )}
+          </div>
+        ))}
       </div>
     </>
   );
