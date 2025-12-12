5d92368cc1111340c2fc0a86ca56425f8320f07f
Rishi <rishikm215@gmail.com>
2025-12-12 14:03:43 +0530

    false commit

--- server/index.js
+++ server/index.js
@@ -17,13 +17,12 @@ const connectDB=async()=>{
     }
 }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "https://syranx.vercel.app/", 
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://syranx.vercel.app/",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 connectDB();
 
