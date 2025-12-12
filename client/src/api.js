9edaf15f5cec6dd90a5dc2b844fa9eee94a1c4fb
Rishi <rishikm215@gmail.com>
2025-12-12 13:43:19 +0530

    backend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://syranx-production.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
