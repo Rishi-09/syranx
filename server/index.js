b65721a1b85812311384f4b6b2b8a031c3fdc2b1
Rishi <rishikm215@gmail.com>
2025-12-12 08:55:45 +0530

    deployment changes

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import getResponse from "./utils/groqClients.js";
+import mongoose from "mongoose";
+import 'dotenv/config';
+import chatRoutes from './routes/chat.js';
+import authRoutes from './routes/auth.js';
+
+const app = express();
+const port = 8080;
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("connected with database!")
+    }catch(err){
+        console.log("connectioon failed!",err);
+    }
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite or React dev server
+    credentials: true,
+  })
+);
+app.use(express.json());
+connectDB();
+
+
+
+app.use("/api",authRoutes);
+app.use("/api",chatRoutes);
+
+
+app.listen(port, () => {
+  console.log(`listening on port localhost:${port} with cors`);
+  
+});
+
